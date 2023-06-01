9d8c3d3d90523da043af68293d2be840754bcb6c
vishalag53 <vishal.agrawal1207@gmail.com>
2023-06-01 15:58:14 +0530

    Step 2: Add SleepDatabaseDao.kt

--- app/src/main/java/com/example/sleeptracker/database/SleepDatabaseDao.kt
+++ app/src/main/java/com/example/sleeptracker/database/SleepDatabaseDao.kt
@@ -1,9 +1,27 @@
 package com.example.sleeptracker.database
 
-import androidx.room.Dao
+import androidx.lifecycle.LiveData
+import androidx.room.*
 
 @Dao
 interface SleepDatabaseDao {
 
+    @Insert
+    fun insert(night: SleepNight)
+
+    @Update
+    fun update(night: SleepNight)
+
+    @Query("SELECT * from daily_sleep_quality_table WHERE nightId = :key")
+    fun get(key: Long): SleepNight
+
+    @Query("DELETE FROM daily_sleep_quality_table")
+    fun clear()
+
+    @Query("SELECT * from daily_sleep_quality_table ORDER BY nightId DESC")
+    fun  getAllNights(): LiveData<List<SleepNight>>
+
+    @Query("SELECT * FROM daily_sleep_quality_table ORDER BY nightId DESC LIMIT 1")
+    fun getTonight(): SleepNight?
 
 }
