926f8f48d755129154b1a43e3c3775a5d17c66df
vishalag53 <vishal.agrawal1207@gmail.com>
2023-06-01 16:27:59 +0530

    Step 3: Add Create_RoomDatabase

--- app/src/main/java/com/example/sleeptracker/database/SleepDatabaseDao.kt
+++ app/src/main/java/com/example/sleeptracker/database/SleepDatabaseDao.kt
@@ -19,7 +19,7 @@ interface SleepDatabaseDao {
     fun clear()
 
     @Query("SELECT * from daily_sleep_quality_table ORDER BY nightId DESC")
-    fun  getAllNights(): LiveData<List<SleepNight>>
+    fun getAllNights(): LiveData<List<SleepNight>>
 
     @Query("SELECT * FROM daily_sleep_quality_table ORDER BY nightId DESC LIMIT 1")
     fun getTonight(): SleepNight?
