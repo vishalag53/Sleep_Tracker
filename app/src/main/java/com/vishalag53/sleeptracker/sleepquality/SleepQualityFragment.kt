a3f0d98f060b48d56b1d5510310e755d4309ca3e
vishalag53 <vishal.agrawal1207@gmail.com>
2023-06-05 14:21:11 +0530

    Step 06 Record Sleep Quality part 2

--- app/src/main/java/com/vishalag53/sleeptracker/sleepquality/SleepQualityFragment.kt
+++ app/src/main/java/com/vishalag53/sleeptracker/sleepquality/SleepQualityFragment.kt
@@ -6,8 +6,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.vishalag53.sleeptracker.R
+import com.vishalag53.sleeptracker.database.SleepDatabase
 import com.vishalag53.sleeptracker.databinding.FragmentSleepQualityBinding
+import com.vishalag53.sleeptracker.sleeptracker.SleepTrackerViewModelFactory
 
 class SleepQualityFragment : Fragment() {
 
@@ -27,7 +32,23 @@ class SleepQualityFragment : Fragment() {
 
         val application = requireNotNull(this.activity).application
 
+        val arguments = SleepQualityFragmentArgs.fromBundle(requireArguments())
 
+        val dataSource = SleepDatabase.getInstance(application).sleepDatabaseDao
+
+        val viewModelFactory = SleepQualityViewModelFactory(arguments.sleepNightKey,dataSource)
+
+        val sleepQualityViewModel = ViewModelProvider(this,viewModelFactory)[SleepQualityViewModel::class.java]
+
+        binding.sleepQualityViewModel = sleepQualityViewModel
+
+        sleepQualityViewModel.navigateToSleepTracker.observe(this, Observer {
+            if(it == true){
+                this.findNavController().navigate(
+                    SleepQualityFragmentDirections.actionSleepQualityFragmentToSleepTrackerFragment())
+                sleepQualityViewModel.doneNavigating()
+            }
+        })
 
         return binding.root
     }
--- app/src/main/java/com/vishalag53/sleeptracker/sleepquality/SleepQualityViewModelFactory.kt
+++ app/src/main/java/com/vishalag53/sleeptracker/sleepquality/SleepQualityViewModelFactory.kt
@@ -1,2 +1,17 @@
 package com.vishalag53.sleeptracker.sleepquality
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.vishalag53.sleeptracker.database.SleepDatabaseDao
+
+class SleepQualityViewModelFactory (
+    private val sleepNightKey: Long,
+    private val dataSource: SleepDatabaseDao): ViewModelProvider.Factory {
+    @Suppress("unchecked_cast")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if(modelClass.isAssignableFrom(SleepQualityViewModel::class.java)){
+            return SleepQualityViewModel(sleepNightKey,dataSource) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel Class")
+    }
+}
