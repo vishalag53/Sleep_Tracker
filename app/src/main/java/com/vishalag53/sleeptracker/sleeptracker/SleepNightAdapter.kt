48b8ae4ad9655b73bb3074a071a24d62f57af997
vishalag53 <vishal.agrawal1207@gmail.com>
2023-07-02 21:24:55 +0530

    Step 14 Add DiffUtil to Adapter (Refress Data with Diffutil)

--- app/src/main/java/com/vishalag53/sleeptracker/sleeptracker/SleepNightAdapter.kt
+++ app/src/main/java/com/vishalag53/sleeptracker/sleeptracker/SleepNightAdapter.kt
@@ -8,6 +8,8 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.vishalag53.sleeptracker.R
 import com.vishalag53.sleeptracker.TextItemViewHolder
@@ -15,23 +17,17 @@ import com.vishalag53.sleeptracker.convertDurationToFormatted
 import com.vishalag53.sleeptracker.convertNumericQualityToString
 import com.vishalag53.sleeptracker.database.SleepNight
 
-class SleepNightAdapter: RecyclerView.Adapter<SleepNightAdapter.ViewHolder>(){
+class SleepNightAdapter: ListAdapter<SleepNight, SleepNightAdapter.ViewHolder>(SleepNightDiffCallback()){
 
-    var data = listOf<SleepNight>()
-        set(value) {
-            field = value
-            notifyDataSetChanged()
-        }
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         return ViewHolder.from(parent)
     }
 
 
-    override fun getItemCount() = data.size
-
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val item = data[position]
+        val item = getItem(position)
 //        if(item.sleepQuality <= 1){
 //            holder.textView.setTextColor(Color.RED)
 //        }
@@ -72,8 +68,16 @@ class SleepNightAdapter: RecyclerView.Adapter<SleepNightAdapter.ViewHolder>(){
                 return ViewHolder(view)
             }
         }
+    }
+}
 
+class SleepNightDiffCallback: DiffUtil.ItemCallback<SleepNight>(){
+    override fun areItemsTheSame(oldItem: SleepNight, newItem: SleepNight): Boolean {
+        return oldItem.nightId == newItem.nightId
+    }
 
+    override fun areContentsTheSame(oldItem: SleepNight, newItem: SleepNight): Boolean {
+        return oldItem == newItem
     }
 
 }
