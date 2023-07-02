d1cb044a5ef6b78647fa050031656cbfd456672b
vishalag53 <vishal.agrawal1207@gmail.com>
2023-07-02 17:33:15 +0530

    Step 09 Display Data

--- app/src/main/java/com/vishalag53/sleeptracker/sleeptracker/SleepNightAdapter.kt
+++ app/src/main/java/com/vishalag53/sleeptracker/sleeptracker/SleepNightAdapter.kt
@@ -1,17 +1,20 @@
 package com.vishalag53.sleeptracker.sleeptracker
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.vishalag53.sleeptracker.R
 import com.vishalag53.sleeptracker.TextItemViewHolder
 import com.vishalag53.sleeptracker.database.SleepNight
 
 class SleepNightAdapter: RecyclerView.Adapter<TextItemViewHolder>(){
 
     var data = listOf<SleepNight>()
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TextItemViewHolder {
-        TODO("Not yet implemented")
-    }
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
 
     override fun getItemCount() = data.size
 
@@ -19,4 +22,13 @@ class SleepNightAdapter: RecyclerView.Adapter<TextItemViewHolder>(){
         val item = data[position]
         holder.textView.text = item.sleepQuality.toString()
     }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TextItemViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val view = layoutInflater.inflate(R.layout.text_item_view,parent,false) as TextView
+        return TextItemViewHolder(view)
+    }
+
+
+
 }
--- app/src/main/java/com/vishalag53/sleeptracker/sleeptracker/SleepTrackerFragment.kt
+++ app/src/main/java/com/vishalag53/sleeptracker/sleeptracker/SleepTrackerFragment.kt
@@ -39,6 +39,15 @@ class SleepTrackerFragment : Fragment() {
 
         binding.sleepTrackerViewModel = sleepTrackerViewModel
 
+        val adapter = SleepNightAdapter()
+        binding.sleepList.adapter = adapter
+
+        sleepTrackerViewModel.nights.observe(viewLifecycleOwner, Observer {
+            it?.let {
+                adapter.data = it
+            }
+        })
+
         sleepTrackerViewModel.navigateToSleepQuality.observe(viewLifecycleOwner, Observer {
             night ->
             night?.let {
